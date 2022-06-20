eb408c42333746e3a85f1c0c0fe720300ae9d84b
James Foster <james.paul.foster@outlook.com>
2022-06-20 14:11:00 -0500

    Refactored out arena code into arena.py, can also add convex obstacles and view them in arena plot

---
+++ project1/arena.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.stats import qmc
+
+from convex_obstacle import ConvexObstacle
+
+
+class Arena2D:
+    def __init__(self):
+        """Create a 2D box arena on (0,1) x (0,1)."""
+        self.x_min = 0
+        self.x_max = 1
+        self.y_min = 0
+        self.y_max = 1
+
+        self.obstacles = []
+
+    def populate_with_random_obstacles(self, num_obstacles: int, max_obstacle_radius: float):
+        """
+        Populate the arena with num_obstacles convex obstacles of max radius max_obstacles radius.
+
+        We place the generated obstacles according to a scrambled Halton sampler, which has been shown to provide more
+        uniform coverage of the space than uniform random sampling. See https://en.wikipedia.org/wiki/Halton_sequence
+        """
+        halton_sampler = qmc.Halton(d=2, scramble=True)
+        obstacle_locations = halton_sampler.random(num_obstacles)
+
+        for i in range(num_obstacles):
+            # We'll randomly draw the number of vertices per obstacle from the discrete uniform distribution over (3,10)
+            obstacle = ConvexObstacle(obstacle_locations[i], np.random.randint(3, 10), max_obstacle_radius)
+            self.obstacles.append(obstacle)
+
+    def plot(self, fig, ax):
+        """Plot the arena and its obstacles."""
+        ax.set_xlim((self.x_min, self.x_max))
+        ax.set_ylim((self.y_min, self.y_max))
+
+        for obstacle in self.obstacles:
+            fig, ax = obstacle.plot(fig, ax)
+        return fig, ax
+
+
+if __name__ == "__main__":
+    # First, create an arena without obstacles (the plot will be empty!)
+    arena = Arena2D()
+    fig, ax = plt.subplots()
+    fig, ax = arena.plot(fig, ax)
+    plt.show()
+
+    # Next, fill the arena with obstacles
+    arena.populate_with_random_obstacles(20, 0.1)
+    fig, ax = plt.subplots()
+    fig, ax = arena.plot(fig, ax)
+    plt.show()
