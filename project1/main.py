ae50be49a9656f9bfd7b68017872ca5a95a5a31c
James Foster <james.paul.foster@outlook.com>
2022-06-21 15:16:21 -0500

    Second set of constrained experiments complete

--- project1/main.py
+++ project1/main.py
@@ -4,7 +4,7 @@ import numpy as np
 from rrt import RRTPlanner
 
 
-def run_mutliple_unconstrained_rrt(initial_state, goal_state, number_of_runs,
+def run_multiple_unconstrained_rrt(initial_state, goal_state, number_of_runs,
                                    maximum_distance_between_vertices, goal_sample_probability):
     success_array = np.zeros(number_of_runs, dtype=bool)
     iterations_array = np.zeros(number_of_runs)
@@ -12,6 +12,17 @@ def run_mutliple_unconstrained_rrt(initial_state, goal_state, number_of_runs,
         rrt = RRTPlanner(initial_state, goal_state, 0, 0,
                          maximum_distance_between_vertices=maximum_distance_between_vertices,
                          goal_sample_probability=goal_sample_probability)
+        success, iterations = rrt.run()
+        success_array[i] = success
+        iterations_array[i] = iterations
+    return success_array, iterations_array
+
+
+def run_multiple_constrained_rrt(initial_state, goal_state, number_of_runs, number_of_obstacles, max_obstacle_radius):
+    success_array = np.zeros(number_of_runs, dtype=bool)
+    iterations_array = np.zeros(number_of_runs)
+    for i in range(number_of_runs):
+        rrt = RRTPlanner(initial_state, goal_state, number_of_obstacles, max_obstacle_radius)
         success, iterations = rrt.run()
         success_array[i] = success
         iterations_array[i] = iterations
@@ -29,36 +40,41 @@ def run_mutliple_unconstrained_rrt(initial_state, goal_state, number_of_runs,
 # Goal detection eps stays fixed at 1e-2.
 initial_state = np.array([0.1, 0.1])
 goal_state = np.array([0.9, 0.9])
-number_of_runs = 50
+number_of_runs = 20
 
 max_distance_values = [0.1, 0.2, 0.3, 0.4, 0.5]
 goal_sample_probability_values = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
 # Represent results as a matrix, with max_distance_values as rows, and goal_sample_probability_values as columns
-number_of_failures_array = np.zeros((len(max_distance_values), len(goal_sample_probability_values)))
+number_of_successes_array = np.zeros((len(max_distance_values), len(goal_sample_probability_values)))
 average_iterations_array = np.zeros((len(max_distance_values), len(goal_sample_probability_values)))
+dataset_number = 0
 for (i, _) in enumerate(max_distance_values):
     for (j, _) in enumerate(goal_sample_probability_values):
-        success_array, iterations_array = run_mutliple_unconstrained_rrt(initial_state, goal_state, number_of_runs,
-                                                                         maximum_distance_between_vertices=max_distance_values[i],
-                                                                         goal_sample_probability=goal_sample_probability_values[j])
+        print(f"Dataset {dataset_number}")
+        success_array, iterations_array = run_multiple_unconstrained_rrt(initial_state, goal_state, number_of_runs,
+                                                                         maximum_distance_between_vertices=
+                                                                         max_distance_values[i],
+                                                                         goal_sample_probability=
+                                                                         goal_sample_probability_values[j])
         print(success_array)
         print(iterations_array)
         # To get a scalar related to successes, we simply count the number of successes
-        number_of_failures = np.count_nonzero(success_array)
-        number_of_failures_array[i, j] = number_of_failures
+        number_of_successes = np.count_nonzero(success_array)
+        number_of_successes_array[i, j] = number_of_successes
         # To get a scalar related to required iterations, we take the average number of iterations
         average_iterations = np.mean(iterations_array)
         average_iterations_array[i, j] = average_iterations
+        dataset_number += 1
 print("DONE!")
-print(number_of_failures_array)
+print(number_of_successes_array)
 print(average_iterations_array)
 fig, ax = plt.subplots()
-ax.matshow(number_of_failures_array)
+ax.matshow(number_of_successes_array)
 for (i, _) in enumerate(goal_sample_probability_values):
     for (j, _) in enumerate(max_distance_values):
-        score = number_of_failures_array[j, i]
+        score = number_of_successes_array[j, i]
         ax.text(i, j, int(score), va='center', ha='center', c='w')
-ax.set_title("Number of failures")
+ax.set_title("Number of successes")
 ax.set_xlabel("goal sample probability")
 ax.set_xticks(np.arange(len(goal_sample_probability_values)))
 ax.set_xticklabels([str(value) for value in goal_sample_probability_values])
@@ -81,7 +97,69 @@ ax.set_yticks(np.arange(len(max_distance_values)))
 ax.set_yticklabels([str(value) for value in max_distance_values])
 plt.show()
 
-# num_obstacles = 20
-# max_obstacle_radius = 0.1
-# rrt = RRTPlanner(initial_state, goal_state, num_obstacles, max_obstacle_radius)
-# rrt.run()
+# Second set of experiments: RRT with obstacles.
+# Vary number of obstacles and their maximum radius to see the effect:
+#   * number of obstacles (5, 10, 15, 20, 25)
+#   * max obstacle radius (0.01, 0.02, ..., 0.1)
+# Run 50 RRTs for each configuration.
+# Initial and goal states close to (0.1, 0.1) and (0.9, 0.9) -- perturbed to free space if inside an obstacle.
+# Maximum distance between vertices fixed at 0.1.
+# Collision checking stays at same resolution of 1e-3.
+# Goal detection eps stays fixed at 1e-2.
+# Goal sample probability fixed at 0.01
+initial_state = np.array([0.1, 0.1])
+goal_state = np.array([0.9, 0.9])
+number_of_runs = 20
+
+number_of_obstacles_values = [5, 10, 15, 20, 25]
+max_obstacle_radius_values = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1]
+# Represent results as a matrix, with number_of_obstacles_values as rows, and max_obstacles_radius_values as columns
+number_of_successes_array = np.zeros((len(number_of_obstacles_values), len(max_obstacle_radius_values)))
+average_iterations_array = np.zeros((len(number_of_obstacles_values), len(max_obstacle_radius_values)))
+dataset_number = 0
+for (i, _) in enumerate(number_of_obstacles_values):
+    for (j, _) in enumerate(max_obstacle_radius_values):
+        print(f"Dataset {dataset_number}")
+        success_array, iterations_array = run_multiple_constrained_rrt(initial_state, goal_state, number_of_runs,
+                                                                       number_of_obstacles_values[i],
+                                                                       max_obstacle_radius_values[j])
+        print(success_array)
+        print(iterations_array)
+        # To get a scalar related to successes, we simply count the number of successes
+        number_of_successes = np.count_nonzero(success_array)
+        number_of_successes_array[i, j] = number_of_successes
+        # To get a scalar related to required iterations, we take the average number of iterations
+        average_iterations = np.mean(iterations_array)
+        average_iterations_array[i, j] = average_iterations
+        dataset_number += 1
+print("DONE!")
+print(number_of_successes_array)
+print(average_iterations_array)
+fig, ax = plt.subplots()
+ax.matshow(number_of_successes_array)
+for (i, _) in enumerate(max_obstacle_radius_values):
+    for (j, _) in enumerate(number_of_obstacles_values):
+        score = number_of_successes_array[j, i]
+        ax.text(i, j, int(score), va='center', ha='center', c='w')
+ax.set_title("Number of successes")
+ax.set_xlabel("maximum obstacle radius")
+ax.set_xticks(np.arange(len(max_obstacle_radius_values)))
+ax.set_xticklabels([str(value) for value in max_obstacle_radius_values])
+ax.set_ylabel("number of obstacles")
+ax.set_yticks(np.arange(len(number_of_obstacles_values)))
+ax.set_yticklabels([str(value) for value in number_of_obstacles_values])
+plt.show()
+fig, ax = plt.subplots()
+ax.matshow(average_iterations_array)
+for (i, _) in enumerate(max_obstacle_radius_values):
+    for (j, _) in enumerate(number_of_obstacles_values):
+        score = average_iterations_array[j, i]
+        ax.text(i, j, f"{score:.2f}", va='center', ha='center', c='w')
+ax.set_title("Average iterations")
+ax.set_xlabel("maximum obstacle radius")
+ax.set_xticks(np.arange(len(max_obstacle_radius_values)))
+ax.set_xticklabels([str(value) for value in max_obstacle_radius_values])
+ax.set_ylabel("number of obstacles")
+ax.set_yticks(np.arange(len(number_of_obstacles_values)))
+ax.set_yticklabels([str(value) for value in number_of_obstacles_values])
+plt.show()
