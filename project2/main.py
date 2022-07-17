b7f4d0770cf9f67fcd5d78827457476ab91e97b0
James Foster <james.paul.foster@outlook.com>
2022-07-17 16:05:20 -0500

    Added function to randomly sample joint configurations within joint limits

--- project2/main.py
+++ project2/main.py
@@ -1,6 +1,7 @@
 import pybullet
 import pybullet_data
 import time
+import numpy as np
 
 # Set up pybullet instance
 physicsClient = pybullet.connect(pybullet.GUI)
@@ -17,17 +18,38 @@ plane = pybullet.loadURDF("plane.urdf")
 box1 = pybullet.loadURDF("assets/box/box.urdf", basePosition=[1, 1, 0.5], useFixedBase=True)
 box2 = pybullet.loadURDF("assets/box/box.urdf", basePosition=[2, 0, 1.0], useFixedBase=True)
 
+def sample_random_joint_configuration(robot):
+    number_of_joints = pybullet.getNumJoints(robot)
+    print(f"Number of joints: {number_of_joints}")
+    random_joint_configuration = []
+    for joint in range(number_of_joints):
+        # Find upper and lower limits of the joint under consideration
+        lower_limit = pybullet.getJointInfo(robot, joint)[8]  # check pybullet docs for getJointInfo(), [8] returns the lower limit from the list
+        upper_limit = pybullet.getJointInfo(robot, joint)[9]  # check pybullet docs for getJointInfo(), [9] returns the upper limit from the list
+        print(f"(Lower, Upper) limits of joint {joint}: ({lower_limit}, {upper_limit})")
+        # Now, randomly sample a joint position within those limits
+        position = np.random.uniform(lower_limit, upper_limit)
+        print(position)
+        random_joint_configuration.append(position)
+    return random_joint_configuration
+
 start_time = 0.0
 end_time = 10.0
 time_step = 0.01
 number_of_simulation_steps = int((end_time - start_time) / time_step)
 
 for t in range(number_of_simulation_steps):
-    # Can either step the simulation (nots sure we need to do this, as only using kinematics)
-    pybullet.stepSimulation()
-    time.sleep(time_step)
-    # Or can just sleep
-    time.sleep(5)
+    print(f"Simulation step: {t} of {number_of_simulation_steps}")
+    # # Can either step the simulation (nots sure we need to do this, as only using kinematics)
+    # pybullet.stepSimulation()
+    # time.sleep(time_step)
+    # # Or can just sleep
+    # time.sleep(5)
+    random_joint_configuration = sample_random_joint_configuration(kuka)
+    number_of_joints = pybullet.getNumJoints(kuka)
+    for joint in range(number_of_joints):
+        pybullet.resetJointState(kuka, joint, random_joint_configuration[joint])
+    time.sleep(0.5)
 
 # End
 pybullet.disconnect()
