0a705c98cd8ef1883f0de79daa6cdc92a7c3ed8e
James Foster <james.paul.foster@outlook.com>
2022-07-20 14:57:50 -0500

    Added investigation of varying norm types and distance thresholds

--- project2/main.py
+++ project2/main.py
@@ -1,18 +1,8 @@
-import pybullet
-import time
 import numpy as np
 import matplotlib.pyplot as plt
 
 from arena import Arena
 from rrt import JointSpaceRRT
-
-
-# TODO: VARY THESE IN YOUR EXPERIMENTS!
-# max_iterations = 1000
-# goal_sample_probability = 0.02
-# use_angular_difference = True
-# norm_type = 2
-# distance_threshold = 4
 
 
 def run_multiple_joint_space_rrt(number_of_runs, number_of_boxes, goal_sample_probability, norm_type, distance_threshold, use_angular_difference):
@@ -169,9 +159,83 @@ def investigate_effect_of_angular_difference():
     print(mean_proportion_of_time_spent_finding_valid_joint_configuration_array)
 
 
+def investigate_effect_of_distance_threshold_for_given_norm(norm_type, distance_threshold_values):
+    number_of_boxes = 10
+    goal_sample_probability = 0.02
+    use_angular_difference = True
+
+    number_of_runs = 20
+    number_of_successes_array = []
+    mean_total_rrt_time_array = []
+    median_total_rrt_time_array = []
+    mean_total_find_valid_joint_configuration_time_array = []
+    mean_proportion_of_time_spent_finding_valid_joint_configuration_array = []
+    for value in distance_threshold_values:
+        print(f"DISTANCE THRESHOLD: {value}")
+        run_stats = run_multiple_joint_space_rrt(number_of_runs, number_of_boxes, goal_sample_probability, norm_type, value, use_angular_difference)
+        number_of_successes_array.append(run_stats[0])
+        mean_total_rrt_time_array.append(run_stats[1])
+        median_total_rrt_time_array.append(run_stats[2])
+        mean_total_find_valid_joint_configuration_time_array.append(run_stats[3])
+        mean_proportion_of_time_spent_finding_valid_joint_configuration_array.append(run_stats[4])
+
+    fig, ax = plt.subplots()
+    ax.scatter(distance_threshold_values, number_of_successes_array, c='b')
+    ax.grid()
+    ax.set_title("Number of successes")
+    ax.set_xlabel("distance threshold")
+    ax.set_ylabel("number of successes")
+    ax.set_yticks(np.arange(0, number_of_runs+5))
+    ax.axhline(number_of_runs, linestyle='-.', c='k')
+    plt.xscale("log")
+    ax.set_xticks(distance_threshold_values)
+    ax.set_xticklabels(distance_threshold_values)
+    plt.show()
+
+    fig, ax = plt.subplots()
+    ax.scatter(distance_threshold_values, mean_total_rrt_time_array, c='b')
+    ax.grid()
+    ax_mirror = ax.twinx()
+    ax_mirror.scatter(distance_threshold_values, median_total_rrt_time_array, c='r', marker='x')
+    ax.set_title("Total rrt time statistics")
+    ax.set_xlabel("distance threshold")
+    ax.set_ylabel("average total rrt time [s]", c='b')
+    ax_mirror.set_ylabel("median total rrt time [s]", c='r')
+    plt.xscale("log")
+    ax.set_xticks(distance_threshold_values)
+    ax.set_xticklabels(distance_threshold_values)
+    plt.show()
+
+    fig, ax = plt.subplots()
+    table_data = [["Distance threshold", "Proportion of time"]]
+    for i in range(len(distance_threshold_values)):
+        table_data.append([distance_threshold_values[i], round(mean_proportion_of_time_spent_finding_valid_joint_configuration_array[i], 2)])
+    table = ax.table(cellText=table_data, loc='center', cellLoc='center')
+    ax.axis('off')
+    fig.tight_layout()
+    plt.show()
+
+    print(number_of_successes_array)
+    print(mean_total_rrt_time_array)
+    print(mean_total_find_valid_joint_configuration_time_array)
+    print(mean_proportion_of_time_spent_finding_valid_joint_configuration_array)
+
+
 # Investigate varying of goal sample probability
 # goal_sample_probability_values = [0.02, 0.04, 0.06, 0.08, 0.1]
 # investigate_effect_of_goal_sample_probability(goal_sample_probability_values)
 
 # Investigate varying use of angular difference
-investigate_effect_of_angular_difference()
+# investigate_effect_of_angular_difference()
+
+# Investigate the use of the 2 norm in distance thresholding
+distance_threshold_values = [0.25, 0.5, 1, 2, 4, 8, 16, 32]
+investigate_effect_of_distance_threshold_for_given_norm(2, distance_threshold_values)
+
+# Investigate the use of the 1 norm in distance thresholding
+distance_threshold_values = [0.25, 0.5, 1, 2, 4, 8, 16, 32]
+investigate_effect_of_distance_threshold_for_given_norm(1, distance_threshold_values)
+
+# Investigate the use of the inf norm in distance thresholding
+distance_threshold_values = [0.25, 0.5, 1, 2, 4, 8, 16, 32]
+investigate_effect_of_distance_threshold_for_given_norm(np.inf, distance_threshold_values)
